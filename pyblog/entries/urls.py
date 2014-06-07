acd26b1a77318bc9f1e3d65196835064fce8ba3c
y-tsutsumi <tsutsu0928@gmail.com>
2014-06-08 05:43:19 +0900

    削除ページを作成．
    あわせて，テンプレートのhtmlのファイル名を規定の名前に変更．

--- pyblog/entries/urls.py
+++ pyblog/entries/urls.py
@@ -1,7 +1,7 @@
 ﻿# coding: utf-8
 
 from django.conf.urls import patterns, include, url
-from django.views.generic import ListView, CreateView, DetailView, UpdateView
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from entries.models import Entry
 from entries.forms import HTML5Form
@@ -10,22 +10,28 @@ urlpatterns = patterns('entries.views',
     url(r'^$',
         ListView.as_view(
             queryset = Entry.objects.order_by('-pub_date'),
-            template_name = 'entries/list.html')),
+            template_name = 'entries/entry_list.html')),
     url(r'^create/$',
         CreateView.as_view(
             model = Entry,
             success_url = '/entries/',
-            template_name = 'entries/update.html',
+            template_name = 'entries/entry_form.html',
             form_class = HTML5Form)),
     url(r'^detail/(?P<pk>\d+)/$',
         DetailView.as_view(
             model = Entry,
-            template_name = 'entries/detail.html'),
+            template_name = 'entries/entry_detail.html'),
         name = "entry_detail"),
     url(r'^update/(?P<pk>\d+)/$',
         UpdateView.as_view(
             model = Entry,
-            template_name = 'entries/update.html')),
+            template_name = 'entries/entry_form.html',
+            form_class = HTML5Form)),
+    url(r'^delete/(?P<pk>\d+)/$',
+        DeleteView.as_view(
+            model = Entry,
+            success_url = '/entries/',
+            template_name = 'entries/entry_confirm_delete.html')),
 )
 
 urlpatterns += staticfiles_urlpatterns()
